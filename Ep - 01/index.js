719ab05e51c4198189d8898c05e4984559f0f94c
Bhargav Talpada <119996397+Bhuro11@users.noreply.github.com>
2024-02-05 17:29:25 +0530

    Add files via upload

---
+++ Ep - 01/index.js
@@ -0,0 +1,19 @@
+const parent = React.createElement('div', {id: 'parent'}, 
+    [
+        React.createElement('div', {id: 'child1'}, 
+            [
+                React.createElement('h1', {id: "heading", className: "heading"}, "I'm Bhargav Talpada and i'm a React Developer."),
+                React.createElement('h2', {id: "heading", className: "heading"}, "This is a Sibling of H1 tag.")
+            ]
+        ),
+        React.createElement('div', {id: 'child2'}, 
+            [
+                React.createElement('h1', {id: "heading", className: "heading"}, "I'm Bhargav Talpada and i'm a React Developer."),
+                React.createElement('h2', {id: "heading", className: "heading"}, "This is a Sibling of H1 tag.")
+            ]
+        )
+    ]
+)
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(parent)
+console.log(parent);
