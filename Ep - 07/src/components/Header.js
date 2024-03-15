02bfbf4c4312fd1283f535a715d01e11bd6cc5d5
Bhuro11 <bhargavtalpada2004@gmail.com>
2024-03-16 00:12:16 +0530

    Ep 7 setup

---
+++ Ep - 07/src/components/Header.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react"
+import { LOGO_URL } from "../utils/constants";
+
+export const Header = () => {
+
+    const [btnName, setBtnName] = useState('LogIn')
+
+    
+    return(
+        <div className="header">
+            <div className="">
+                <img className="logo" src={LOGO_URL} />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Cart</li>
+                    <button onClick={()=>{btnName === 'LogIn' ? setBtnName("LogOut") : setBtnName("LogIn")}} className="btnlogin">{btnName}</button>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
---
+++ Ep - 07/src/components/ShimmerUI.js
@@ -0,0 +1,151 @@
+const Shimmer = () => {
+    return(
+        <div >
+            <button className="shimmer-btn-filter"></button>
+            <div className="shimmer-container">
+                <div className="shimmer-cards">
+                    <img className="cart-img" />
+                    <h3 className="shimmer-h3"></h3>
+                    <h4 className="shimmer-h4"></h4>
+                    <h4 className="shimmer-h4"></h4>
+                    <h4 className="shimmer-h4"></h4>
+                </div>
+                <div className="shimmer-cards">
+                    <img className="cart-img" />
+                    <h3 className="shimmer-h3"></h3>
+                    <h4 className="shimmer-h4"></h4>
+                    <h4 className="shimmer-h4"></h4>
+                    <h4 className="shimmer-h4"></h4>
+                </div>
+                <div className="shimmer-cards">
+                    <img className="cart-img" />
+                    <h3 className="shimmer-h3"></h3>
+                    <h4 className="shimmer-h4"></h4>
+                    <h4 className="shimmer-h4"></h4>
+                    <h4 className="shimmer-h4"></h4>
+                </div>
+                <div className="shimmer-cards">
+                    <img className="cart-img" />
+                    <h3 className="shimmer-h3"></h3>
+                    <h4 className="shimmer-h4"></h4>
+                    <h4 className="shimmer-h4"></h4>
+                    <h4 className="shimmer-h4"></h4>
+                </div>
+                <div className="shimmer-cards">
+                    <img className="cart-img" />
+                    <h3 className="shimmer-h3"></h3>
+                    <h4 className="shimmer-h4"></h4>
+                    <h4 className="shimmer-h4"></h4>
+                    <h4 className="shimmer-h4"></h4>
+                </div>
+                <div className="shimmer-cards">
+                    <img className="cart-img" />
+                    <h3 className="shimmer-h3"></h3>
+                    <h4 className="shimmer-h4"></h4>
+                    <h4 className="shimmer-h4"></h4>
+                    <h4 className="shimmer-h4"></h4>
+                </div>
+                <div className="shimmer-cards">
+                    <img className="cart-img" />
+                    <h3 className="shimmer-h3"></h3>
+                    <h4 className="shimmer-h4"></h4>
+                    <h4 className="shimmer-h4"></h4>
+                    <h4 className="shimmer-h4"></h4>
+                </div>
+                <div className="shimmer-cards">
+                    <img className="cart-img" />
+                    <h3 className="shimmer-h3"></h3>
+                    <h4 className="shimmer-h4"></h4>
+                    <h4 className="shimmer-h4"></h4>
+                    <h4 className="shimmer-h4"></h4>
+                </div>
+                <div className="shimmer-cards">
+                    <img className="cart-img" />
+                    <h3 className="shimmer-h3"></h3>
+                    <h4 className="shimmer-h4"></h4>
+                    <h4 className="shimmer-h4"></h4>
+                    <h4 className="shimmer-h4"></h4>
+                </div>
+                <div className="shimmer-cards">
+                    <img className="cart-img" />
+                    <h3 className="shimmer-h3"></h3>
+                    <h4 className="shimmer-h4"></h4>
+                    <h4 className="shimmer-h4"></h4>
+                    <h4 className="shimmer-h4"></h4>
+                </div>
+                <div className="shimmer-cards">
+                    <img className="cart-img" />
+                    <h3 className="shimmer-h3"></h3>
+                    <h4 className="shimmer-h4"></h4>
+                    <h4 className="shimmer-h4"></h4>
+                    <h4 className="shimmer-h4"></h4>
+                </div>
+                <div className="shimmer-cards">
+                    <img className="cart-img" />
+                    <h3 className="shimmer-h3"></h3>
+                    <h4 className="shimmer-h4"></h4>
+                    <h4 className="shimmer-h4"></h4>
+                    <h4 className="shimmer-h4"></h4>
+                </div>
+                <div className="shimmer-cards">
+                    <img className="cart-img" />
+                    <h3 className="shimmer-h3"></h3>
+                    <h4 className="shimmer-h4"></h4>
+                    <h4 className="shimmer-h4"></h4>
+                    <h4 className="shimmer-h4"></h4>
+                </div>
+                <div className="shimmer-cards">
+                    <img className="cart-img" />
+                    <h3 className="shimmer-h3"></h3>
+                    <h4 className="shimmer-h4"></h4>
+                    <h4 className="shimmer-h4"></h4>
+                    <h4 className="shimmer-h4"></h4>
+                </div>
+                <div className="shimmer-cards">
+                    <img className="cart-img" />
+                    <h3 className="shimmer-h3"></h3>
+                    <h4 className="shimmer-h4"></h4>
+                    <h4 className="shimmer-h4"></h4>
+                    <h4 className="shimmer-h4"></h4>
+                </div>
+                <div className="shimmer-cards">
+                    <img className="cart-img" />
+                    <h3 className="shimmer-h3"></h3>
+                    <h4 className="shimmer-h4"></h4>
+                    <h4 className="shimmer-h4"></h4>
+                    <h4 className="shimmer-h4"></h4>
+                </div>
+                <div className="shimmer-cards">
+                    <img className="cart-img" />
+                    <h3 className="shimmer-h3"></h3>
+                    <h4 className="shimmer-h4"></h4>
+                    <h4 className="shimmer-h4"></h4>
+                    <h4 className="shimmer-h4"></h4>
+                </div>
+                <div className="shimmer-cards">
+                    <img className="cart-img" />
+                    <h3 className="shimmer-h3"></h3>
+                    <h4 className="shimmer-h4"></h4>
+                    <h4 className="shimmer-h4"></h4>
+                    <h4 className="shimmer-h4"></h4>
+                </div>
+                <div className="shimmer-cards">
+                    <img className="cart-img" />
+                    <h3 className="shimmer-h3"></h3>
+                    <h4 className="shimmer-h4"></h4>
+                    <h4 className="shimmer-h4"></h4>
+                    <h4 className="shimmer-h4"></h4>
+                </div>
+                <div className="shimmer-cards">
+                    <img className="cart-img" />
+                    <h3 className="shimmer-h3"></h3>
+                    <h4 className="shimmer-h4"></h4>
+                    <h4 className="shimmer-h4"></h4>
+                    <h4 className="shimmer-h4"></h4>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Shimmer;
