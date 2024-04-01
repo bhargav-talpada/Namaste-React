8ba3940fbb145acf9b3f5f209166a91ea49666db
Bhuro11 <bhargavtalpada2004@gmail.com>
2024-04-02 01:05:44 +0530

    usestate in class components

--- Ep - 08/src/components/User.js
+++ Ep - 08/src/components/User.js
@@ -1,7 +1,14 @@
+import { useState } from "react";
+
 const User = (props) => {
     const {name, location, contact, email} = props;
+    const [count, setCount] = useState(1)
+    const [count1, setCount1] = useState(2)
     return(
         <div className="user-card">
+            <h3>Count : {count}</h3>
+            <h3>Count1 : {count1}</h3>
+            <button onClick={()=>{setCount(count + 1), setCount1(count1 + 2)}}>Count</button>
             <h1>Name : {name}</h1>
             <h3>Location : {location}</h3>
             <h3>Contact : {contact}</h3>
--- Ep - 08/src/components/UserClass.js
+++ Ep - 08/src/components/UserClass.js
@@ -5,15 +5,29 @@ class UserClass extends React.Component {
     constructor(props){
         super(props);
         
+        this.state = {
+            count: 1,
+            count1: 2,
+        }
     }
 
     render(){
+        const {name, location, contact, email} = this.props;
+        const {count, count1} = this.state;
         return(
             <div className="user-card">
-                <h1>Name : {this.props.name}</h1>
-                <h3>Location : {this.props.location}</h3>
-                <h3>Contact : {this.props.contact}</h3>
-                <h3>Email : {this.props.email}</h3>
+                <h3>Count : {count}</h3>
+                <h3>Count1 : {count1}</h3>
+                <button onClick={()=>{
+                    this.setState({
+                        count: this.state.count + 1,
+                        count1: this.state.count1 + 2
+                    })
+                }}>Count</button>
+                <h1>Name : {name}</h1>
+                <h3>Location : {location}</h3>
+                <h3>Contact : {contact}</h3>
+                <h3>Email : {email}</h3>
             </div>
         )
     }
