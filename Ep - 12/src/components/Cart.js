01d0d2d215ca39c1049e63e2c28cfacff49b6e95
Bhuro11 <bhargavtalpada2004@gmail.com>
2024-04-24 00:12:42 +0530

    msg

--- Ep - 12/src/components/Cart.js
+++ Ep - 12/src/components/Cart.js
@@ -17,7 +17,7 @@ const Cart = () => {
             <h1 className="text-2xl">Cart</h1>
             <div className="w-6/12 m-auto ">
                 <button className="p-2 m-2 bg-black text-white rounded-lg text-xl" onClick={handleClearcart}>Clear Cart</button>
-                {cartItems.length === 0 && <h1 className="text-xl mt-10 text-red-500">Go & Do Some More Shopping.</h1>} 
+                {cartItems.length === 0 && <h1 className="text-xl mt-10 text-red-500">Go & Do Some More Shopping...</h1>} 
                 <ItemList items={cartItems} />
             </div>
         </div>
