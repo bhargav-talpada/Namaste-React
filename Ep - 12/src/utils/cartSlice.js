ca8b9d037d4e4aa3147a3dbe916ac6b827e68608
Bhuro11 <bhargavtalpada2004@gmail.com>
2024-04-24 00:50:03 +0530

    notes

--- Ep - 12/src/utils/cartSlice.js
+++ Ep - 12/src/utils/cartSlice.js
@@ -13,7 +13,7 @@ const cartSlice = createSlice({
             state.items.pop();
         },
         clearCart: (state) => {
-            state.items.length = 0;
+            state.items.length = 0;  //state = []
         },
     },
 });
