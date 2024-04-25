f2e7a4f15813525dd6a70d24d16cf1d563beb1aa
Bhuro11 <bhargavtalpada2004@gmail.com>
2024-04-26 02:19:37 +0530

    it and test

--- Ep - 13/src/components/__tests__/Contact.test.js
+++ Ep - 13/src/components/__tests__/Contact.test.js
@@ -2,49 +2,49 @@ import Contact from "../Contact";
 import {render,screen} from "@testing-library/react";
 import "@testing-library/jest-dom";
 
-test('should load contact page', () => { 
-    render(<Contact />);
+describe("Contact Us Page Test Cases", () => {
 
-    const heading = screen.getByRole("heading");
+    test('should load contact page', () => { 
+        render(<Contact />);
+    
+        const heading = screen.getByRole("heading");
+    
+        // Assertion
+        expect(heading).toBeInTheDocument();
+    });
+    
+    test('should load button inside contact component ', () => { 
+        render(<Contact />)
+    
+        const btn = screen.getByRole("button");
+    
+        expect(btn).toBeInTheDocument();
+    });
+    
+    test('should load text inside contact component', () => { 
+        render(<Contact />);
+    
+        const text = screen.getByText('Submit');
+    
+        expect(text).toBeInTheDocument();
+    });
+    
+    it('should load placeholder text inside contact component', () => { 
+        render(<Contact />);
+    
+        const placeholderText = screen.getByPlaceholderText("Enter Your Name");
+    
+        expect(placeholderText).toBeInTheDocument();
+    });
+    
+    it('should load 2 input boxes in contact component', () => { 
+        render(<Contact />);
+    
+        const inputBoxes = screen.getAllByRole("textbox");
+    
+        // console.log(inputBoxes.length);
+    
+        expect(inputBoxes.length).toBe(4);
+    });
 
-    // Assertion
-    expect(heading).toBeInTheDocument();
-});
-
-
-test('should load button inside contact component ', () => { 
-    render(<Contact />)
-
-    const btn = screen.getByRole("button");
-
-    expect(btn).toBeInTheDocument();
-});
-
-
-test('should load text inside contact component', () => { 
-    render(<Contact />);
-
-    const text = screen.getByText('Submit');
-
-    expect(text).toBeInTheDocument();
-});
-
-
-test('should load placeholder text inside contact component', () => { 
-    render(<Contact />);
-
-    const placeholderText = screen.getByPlaceholderText("Enter Your Name");
-
-    expect(placeholderText).toBeInTheDocument();
-});
-
-
-test('should load 2 input boxes in contact component', () => { 
-    render(<Contact />);
-
-    const inputBoxes = screen.getAllByRole("textbox");
-
-    // console.log(inputBoxes.length);
-
-    expect(inputBoxes.length).toBe(4);
 });
