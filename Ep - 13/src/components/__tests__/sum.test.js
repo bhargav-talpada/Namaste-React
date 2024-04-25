7a7f4821f561b7e96f99a182c5e1354b9e4e0c43
Bhuro11 <bhargavtalpada2004@gmail.com>
2024-04-26 00:37:36 +0530

    practise

--- Ep - 13/src/components/__tests__/sum.test.js
+++ Ep - 13/src/components/__tests__/sum.test.js
@@ -1,9 +1,15 @@
-import { sum } from "../sum"
+import { div, mul, sub, sum } from "../sum"
 
 
 test('Sum function should calculate the sum of two numbers', () => {
-    const result = sum(24,72);
+    const addres = sum(24,72);
+    const subres = sub(71,35);
+    const mulres = mul(20,4);
+    const divres = div(80,4);
 
     // Below line is known as Assertion
-    expect(result).toBe(96);
+    expect(addres).toBe(96);
+    expect(subres).toBe(36);
+    expect(mulres).toBe(80);
+    expect(divres).toBe(20);
 })
