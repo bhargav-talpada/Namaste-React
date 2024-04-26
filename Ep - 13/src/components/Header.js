d7bd57e02af872eb601d33ee9321182346f29929
Bhuro11 <bhargavtalpada2004@gmail.com>
2024-04-26 15:36:57 +0530

    header testing done

--- Ep - 13/src/components/Header.js
+++ Ep - 13/src/components/Header.js
@@ -15,7 +15,7 @@ export const Header = () => {
     const {loggedInUser} = useContext(UserContext);
 
     const cartItems = useSelector((store) => store.cart.items)
-    console.log(cartItems);
+    // console.log(cartItems);
 
     return(
         <div className="header flex justify-between items-center bg-pink-100 shadow-xl m-2 sm:bg-yellow-100">
